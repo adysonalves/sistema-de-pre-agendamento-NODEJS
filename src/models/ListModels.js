9decc9b40cbe295ad6f312426353e18227c495fc
adysonalves <adyson6@gmail.com>
2023-08-18 15:40:15 -0300

    Listagem dos Models

---
+++ src/models/ListModels.js
@@ -0,0 +1,7 @@
+function ListModels(){
+    const Cliente = require('./Cliente');
+    const Especialidade = require('./Especialidade');
+    const Agendamento = require('./Agendamento');
+}
+
+module.exports = ListModels;
